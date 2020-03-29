838523e839ab66ac6f183ed782ca016f1a48032e
Valerie <daffaraihanzaki@gmail.com>
2020-03-29 18:30:00 +0700

    Updates

---
+++ precache-manifest.d153ad40f7e3ca1e149e0e7f00359028.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf792cc0915330e07015e7fca48cb1b",
+    "url": "/amark/index.html"
+  },
+  {
+    "revision": "38d31c9bfc3f18286193",
+    "url": "/amark/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "40238afaf727fed00fb6",
+    "url": "/amark/static/css/main.d3e7d653.chunk.css"
+  },
+  {
+    "revision": "38d31c9bfc3f18286193",
+    "url": "/amark/static/js/2.d9f64fd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amark/static/js/2.d9f64fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40238afaf727fed00fb6",
+    "url": "/amark/static/js/main.8bf65c23.chunk.js"
+  },
+  {
+    "revision": "7bb84d11a466db72c46b",
+    "url": "/amark/static/js/runtime-main.69cfc581.js"
+  },
+  {
+    "revision": "16aba5e0cea275ffb4633c2b7bc40a2b",
+    "url": "/amark/static/media/AMARK.16aba5e0.svg"
+  },
+  {
+    "revision": "1e6670924f92307f92f5ad8574897049",
+    "url": "/amark/static/media/arrowleft.1e667092.svg"
+  },
+  {
+    "revision": "7442b64020766a89b881ecf555ec1f1f",
+    "url": "/amark/static/media/art1.7442b640.png"
+  },
+  {
+    "revision": "4e23273288ba0b3018657a016172c469",
+    "url": "/amark/static/media/art2.4e232732.png"
+  },
+  {
+    "revision": "716afedae3ec6d2742f179b10eff1251",
+    "url": "/amark/static/media/art3.716afeda.png"
+  },
+  {
+    "revision": "8f999fd89dac0fa73486a131f208c824",
+    "url": "/amark/static/media/art4.8f999fd8.png"
+  },
+  {
+    "revision": "581132e71f05869418ccce0152b6c3f0",
+    "url": "/amark/static/media/art5.581132e7.png"
+  },
+  {
+    "revision": "592f2e0a0325813db45912e0f5595fac",
+    "url": "/amark/static/media/art6.592f2e0a.png"
+  },
+  {
+    "revision": "cba10ea2a35fa70bdb6ac0db17acc604",
+    "url": "/amark/static/media/contact.cba10ea2.png"
+  },
+  {
+    "revision": "59c44b1eade912d0adc4b33319c96d69",
+    "url": "/amark/static/media/dots.59c44b1e.svg"
+  },
+  {
+    "revision": "4d8ea070ec77010cb54449d031671a8d",
+    "url": "/amark/static/media/headline-img.4d8ea070.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amark/static/media/logo.5d5d9eef.svg"
+  }
+]);
